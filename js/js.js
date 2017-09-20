dd4d812fab6ee55f4dbbf5ba38ee29689707b4af
MaximeBastion <223330@supinfo.com>
2017-09-20 21:48:51 +0200

    Apparitions animation

---
+++ js/js.js
@@ -0,0 +1,12 @@
+
+/* Apparition animation */
+$(window).scroll(function() {
+    $(".slideanim").each(function(){
+        var pos = $(this).offset().top;
+
+        var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+            $(this).addClass("slide");
+        }
+    });
+});
