f4323dd225627c5c68f3833aae57cec29f66eecc
MaximeBastion <223330@supinfo.com>
2017-09-22 21:43:30 +0200

    Revert "Modern v2"
    This reverts commit 15ed5c7af50c85a719a94d0734b86faf0bece164.

--- js/js.js
+++ js/js.js
@@ -1,29 +1,12 @@
 
-
 /* Apparition animation */
-$(window).scroll(function() {   // only for bottom to top entrance
+$(window).scroll(function() {
     $(".slideanim").each(function(){
         var pos = $(this).offset().top;
 
         var winTop = $(window).scrollTop();
         if (pos < winTop + 600) {
             $(this).addClass("slide");
-        }
-    });
-});
-$(window).scroll(function() { // Slides in animated elements according to their position
-    $(".animated").each(function(){
-        var pos = $(this).offset().top;
-        var winTop = $(window).scrollTop();
-        var winMid = $(window).width()/2;
-        if (pos < winTop + 600) {
-            $(this).css('visibility', 'visible');
-            if ($(this).offset().left < winMid) {
-                $(this).addClass("fadeInLeft");
-            } else {
-                $(this).addClass("fadeInRight");
-            }
-
         }
     });
 });
