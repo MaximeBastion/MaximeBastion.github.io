5ddb1596408ac8a5ee36553fceaa05d48ee223c6
Maxime <223330@supinfo.com>
2017-10-05 11:58:15 +0200

    to laptop

--- js/contact.js
+++ js/contact.js
@@ -1,22 +1,37 @@
-
-
+// Open ?
 function isOpen(openDays, openTimes) {
     var date = new Date();
     var day = date.getDay();
     var hour = date.getHours();
     if (openDays.indexOf(day) !== -1) {    /* Sur un jour de travail ? */
         var hours = openTimes[day];
-        if (betweenHours(hour, openTimes[day])) {
+        if (betweenHours(date, hours)) {
             return true;
         }
     }
     return false;
 }
 
-function betweenHours(hour, hours) {
-    return (hour >= hours[0] && hour < hours[1]);
+function betweenHours(date, hours) {
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    var hm = hour + min/100;
+    var hmStart = hours[0] + hours[1]/100;
+    var hmStop = hours[2] + hours[3]/100;
+    return (hm >= hmStart && hm <= hmStop);
 }
 
+
+
 openDays = [1, 2, 3, 4, 5];
-openTimes = {1: [8, 18], 2: [8, 18], 3: [8, 18], 4: [8, 18], 5: [8, 18]};
-alert(isOpen(openDays, openTimes));
+openTimes = {1: [7, 30, 18, 0], 2: [7, 30, 18, 0], 3: [7, 30, 18, 0], 4: [7, 30, 18, 0], 5: [7, 30, 18, 0]};
+openCloseText = $( "span#openClose" );
+if (isOpen(openDays, openTimes)) {
+    openCloseText.html( "Ouvert" );
+    openCloseText.addClass("open");
+
+} else {
+    openCloseText.html( "Fermé" );
+    openCloseText.addClass("closed");
+}
+
