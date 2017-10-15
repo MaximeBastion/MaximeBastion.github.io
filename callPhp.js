7082d017f97dc5057b0c7628b08937f11581bce0
Maxime <223330@supinfo.com>
2017-10-15 20:58:45 +0200

    Some responsive, better homepage

---
+++ callPhp.js
@@ -0,0 +1,15 @@
+/**
+ * Created by maxim on 04/10/2017.
+ */
+
+
+function doSomething() {
+    $.get("somepage.php");
+    return false;
+}
+
+
+// <a href="#" onclick="doSomething();">Click Me!</a>
+function callAutoMail() {
+
+}
